9cfa7e0232626b4e44ea4e928de90d6021a4ef03
isabelle <isabellekrochaamaral@gmail.com>
2026-07-13 09:11:55 -0300

    Gatos fofos

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,17 @@
 import Link from "next/link";
-
+import Image from"next/image";
 export default function Home() {
   return (
     <div>
-      Hello World.{" "}
+      Gatinhos fofos programando.{" "}
+      <br></br>
+       <Image 
+          src="/images/gatofofo.jpg" 
+          alt="Gatinho fofo programando" 
+          width={500} 
+          height={300} 
+        />
+        <br></br>
       <Link href="/about">
         About
       </Link>
